7cdfc089c9e1a9feff8f0a57c25b0437426dd05a
Vinicius Maia <vinicius99santos.vs@gmail.com>
2023-06-07 09:46:25 -0300

    Remove all liquibase files and change the identation

--- src/main/java/com/br/viniciusmaiacommerce/selltime/handler/RestExceptionHandler.java
+++ src/main/java/com/br/viniciusmaiacommerce/selltime/handler/RestExceptionHandler.java
@@ -1,13 +1,18 @@
 package com.br.viniciusmaiacommerce.selltime.handler;
 
-import com.br.viniciusmaiacommerce.selltime.model.error.ErrorMessage;import com.br.viniciusmaiacommerce.selltime.model.exception.ResourceNotFoundException;import org.springframework.http.HttpStatus;import org.springframework.http.ResponseEntity;import org.springframework.web.bind.annotation.ControllerAdvice;
+import com.br.viniciusmaiacommerce.selltime.model.error.ErrorMessage;
+import com.br.viniciusmaiacommerce.selltime.model.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class RestExceptionHandler {
   @ExceptionHandler(ResourceNotFoundException.class)
   public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException exception) {
-    ErrorMessage error = new ErrorMessage("Not Found", HttpStatus.NOT_FOUND.value(), exception.getMessage());
+    ErrorMessage error =
+        new ErrorMessage("Not Found", HttpStatus.NOT_FOUND.value(), exception.getMessage());
     return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
   }
 }
--- src/main/java/com/br/viniciusmaiacommerce/selltime/model/error/ErrorMessage.java
+++ src/main/java/com/br/viniciusmaiacommerce/selltime/model/error/ErrorMessage.java
@@ -13,5 +13,4 @@ public class ErrorMessage {
     this.status = status;
     this.message = message;
   }
-  
 }
